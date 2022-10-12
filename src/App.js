b744bbcab0ab25ba73162354e41a3071dbd916ac
Miguel <miguezaga2@hotmail.com>
2022-10-12 08:54:51 -0500

    setup project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <h1>New App</h1>
+  Reliable, efficient delivery
+  Powered by Technology
+
+  Our Artificial Intelligence powered tools use millions of project data points 
+  to ensure that your project is successful
+
+  Supervisor
+  Monitors activity to identify project roadblocks
+
+  Team Builder
+  Scans our talent network to create the optimal team for your project
+
+  Karma
+  Regularly evaluates our talent to ensure quality
+
+  Calculator
+  Uses data from past projects to provide better delivery estimates
     </div>
   );
 }
