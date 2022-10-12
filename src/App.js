66bc401fb97cc301ce5af6d879f98677039dd3e1
Miguel <miguezaga2@hotmail.com>
2022-10-12 11:10:04 -0500

    add initial jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import { useState } from "react";
 
 function App() {
+  var cardsInfo = [
+    {
+      title: "Supervisor",
+      text: "Monitors activity to identify project roadblocks",
+    },
+    {
+      title: "Team Builder",
+      text: "Scans our talent network to create the optimal team for your project",
+    },
+    {
+      title: "Karma",
+      text: "Regularly evaluates our talent to ensure quality",
+    },
+    {
+      title: "Calculator",
+      text: "Uses data from past projects to provide better delivery estimates",
+    },
+  ];
   return (
-    <div>
-  Reliable, efficient delivery
-  Powered by Technology
+    <main className="feature">
+      <section className="feature__top">
+        <h2 className="feature__subtitle">Reliable, efficient delivery</h2>
+        <h1 className="feature__title"> Powered by Technology</h1>
 
-  Our Artificial Intelligence powered tools use millions of project data points 
-  to ensure that your project is successful
+        <p className="feature__text">
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
+        </p>
+      </section>
 
-  Supervisor
-  Monitors activity to identify project roadblocks
-
-  Team Builder
-  Scans our talent network to create the optimal team for your project
-
-  Karma
-  Regularly evaluates our talent to ensure quality
-
-  Calculator
-  Uses data from past projects to provide better delivery estimates
-    </div>
+      <section className="feature__cards">
+        {cardsInfo.map(function renderCard(cardInfo) {
+          return (
+            <article className="card">
+              <h3 className="card__title">{cardInfo.title}</h3>
+              <p className="card__text">{cardInfo.text}</p>
+            </article>
+          );
+        })}
+      </section>
+    </main>
   );
 }
 
